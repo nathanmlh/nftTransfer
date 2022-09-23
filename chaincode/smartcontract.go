f5621b44761b1a52028357513b374c7a86d0128f
nathanmlh <nathanholdom1@gmail.com>
2022-09-22 21:34:45 -0500

    nft contract
    nft contract

---
+++ chaincode/smartcontract.go
@@ -0,0 +1,80 @@
+package chaincode
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// SmartContract provides functions for managing an Asset
+type SmartContract struct {
+	contractapi.Contract
+}
+
+// Asset describes basic details of what makes up a simple asset
+// Insert struct field in alphabetic order => to achieve determinism across languages
+// golang keeps the order when marshal to json but doesn't order automatically
+type NFTAsset struct {
+	AppraisedValue int    `json:"AppraisedValue"`
+	ID             string `json:"ID"`
+	Owner          string `json:"Owner"`
+	Url            string `json:"Url"`
+}
+
+func (s *SmartContract) CreateNFTAsset(ctx contractapi.TransactionContextInterface, id string, url string, owner string, appraisedValue int) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", id)
+	}
+
+	nft_asset := NFTAsset{
+		ID: id,
+		Url: url,
+		Owner: owner,
+		AppraisedValue: appraisedValue,
+	}
+
+	/// Converts to json
+	assetJSON, err := json.Marshal(nft_asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// ReadAsset returns the asset stored in the world state with given id.
+func (s *SmartContract) ReadNFTAsset(ctx contractapi.TransactionContextInterface, id string) (*NFTAsset, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return nil, fmt.Errorf("the asset %s does not exist", id)
+	}
+
+	var asset NFTAsset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &asset, nil
+}
+
+// DeleteAsset deletes an given asset from the world state.
+func (s *SmartContract) DeleteNFTAsset(ctx contractapi.TransactionContextInterface, id string) error {
+	exists, err := s.AssetExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("the asset %s does not exist", id)
+	}
+
+	return ctx.GetStub().DelState(id)
+}
---
+++ nftTransfer.go
@@ -0,0 +1,25 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+	
+	// We need contract api to make new chaincodes
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/nathanmlh/nftTransfer/chaincode"
+
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
+	}
+}
